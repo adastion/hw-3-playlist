9a13be21108354376c5cb41e5a37963d2402abd0
Andrey Sery <andrey.72006@gmail.com>
2023-12-07 19:56:08 +0300

    new links data

--- src/data.js
+++ src/data.js
@@ -35,7 +35,8 @@ const plalist = {
       songName: "You Can't Touch This",
       year: 1990,
       isHot: false,
-      fileUrl: "https://s2.deliciouspeaches.com/get/cuts/df/f3/dff3416ca0194cf11ab0f6869233379a/47995586/MC_Hammer_-_U_Cant_Touch_This_b128f0d260.mp3",
+      fileUrl:
+        "https://s2.deliciouspeaches.com/get/cuts/df/f3/dff3416ca0194cf11ab0f6869233379a/47995586/MC_Hammer_-_U_Cant_Touch_This_b128f0d260.mp3",
       cover: "https://static.hitmcdn.com/covers/a/c90/9ec/39641.jpg",
       info: {
         duration: "4:16",
@@ -62,7 +63,8 @@ const plalist = {
       songName: "In da Club",
       year: 2003,
       isHot: false,
-      fileUrl: "https://ds.cdn1.deliciouspeaches.com/get/cuts/1d/c9/1dc9bed60952bc86dc754ed6bca8f67d/47965582/50_Cent_-_In_Da_Club_b128f0d193.mp3",
+      fileUrl:
+        "https://ds.cdn1.deliciouspeaches.com/get/cuts/1d/c9/1dc9bed60952bc86dc754ed6bca8f67d/47965582/50_Cent_-_In_Da_Club_b128f0d193.mp3",
       cover: "https://static.hitmcdn.com/covers/a/4d2/e7b/1199.jpg",
       info: {
         duration: "3:13",
@@ -75,7 +77,8 @@ const plalist = {
       songName: "X Gon' Give It To Ya",
       year: 2003,
       isHot: false,
-      fileUrl: "https://ds.cdn13.deliciouspeaches.com/get/cuts/50/db/50db78476930c2bc60c0651c0a01e140/48021216/DMX_-_X_Gon_Give_It_To_Ya_b128f0d217.mp3",
+      fileUrl:
+        "https://ds.cdn13.deliciouspeaches.com/get/cuts/50/db/50db78476930c2bc60c0651c0a01e140/48021216/DMX_-_X_Gon_Give_It_To_Ya_b128f0d217.mp3",
       cover: "https://static.hitmcdn.com/covers/a/955/cb5/2054.jpg",
       info: {
         duration: "3:38",
@@ -90,8 +93,7 @@ const plalist = {
       isHot: false,
       fileUrl:
         "https://ds.cdn9.deliciouspeaches.com/get/cuts/29/2a/292a03ad67db8204aa071c8e3c9baf91/48055043/Eminem_50_Cent_Lloyd_Banks_Cahis_-_You_Dont_Know_b128f0d257.mp3",
-      cover:
-        "https://static.hitmcdn.com/covers/a/a7f/43c/33098.jpg",
+      cover: "https://static.hitmcdn.com/covers/a/a7f/43c/33098.jpg",
       info: {
         duration: "4:17",
         totalTracksCount: 18,
@@ -103,7 +105,8 @@ const plalist = {
       songName: "Rap God",
       year: 2013,
       isHot: true,
-      fileUrl: "https://ds.cdn4.deliciouspeaches.com/get/cuts/53/45/5345f1c087ec56d686a21e7013474e74/47964975/Eminem_-_Rap_God_b128f0d364.mp3",
+      fileUrl:
+        "https://ds.cdn4.deliciouspeaches.com/get/cuts/53/45/5345f1c087ec56d686a21e7013474e74/47964975/Eminem_-_Rap_God_b128f0d364.mp3",
       cover: "https://static.hitmcdn.com/covers/a/0ac/6bb/9957.jpg",
       info: {
         duration: "6:03",
