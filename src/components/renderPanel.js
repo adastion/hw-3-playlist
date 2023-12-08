c79cad4bf5411618f1ec7c298bfe8d0f34e694e7
Andrey Sery <andrey.72006@gmail.com>
2023-12-08 12:26:59 +0300

    adding renderForm, renderPanelButtons

--- src/components/renderPanel.js
+++ src/components/renderPanel.js
@@ -3,17 +3,14 @@ import { renderForm } from "./renderForm.js";
 import { renderPanelButtons } from "./renderPanelButtons.js";
 
 export function renderPanel(parentItem) {
-  const container = containerElement()
+  const container = containerElement();
 
   const panelElement = document.createElement("div");
   panelElement.classList.add("panel");
   container.appendChild(panelElement);
 
-  renderForm(panelElement)
-  renderPanelButtons(panelElement)
-
-
-
+  renderForm(panelElement);
+  renderPanelButtons(panelElement);
 
   parentItem.appendChild(panelElement);
 }
