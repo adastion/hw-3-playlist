e472cebb97387f986fe67bc0f6a0238067863139
Andrey Sery <andrey.72006@gmail.com>
2023-12-10 19:59:09 +0300

    fixing styles

--- src/components/playlist/playlist_wrapper/renderPlaylist.js
+++ src/components/playlist/playlist_wrapper/renderPlaylist.js
@@ -1,4 +1,4 @@
-// import { setTracks } from "../../../data.js";
+import { getPlaylist, setSortSongs } from "../../../data.js";
 import { renderCategories } from "../../cadigories/renderCadigories.js";
 import { containerElement } from "../../containerElement.js";
 import { renderTracks } from "./../../playlist/tracks/renderTracks.js";
@@ -12,57 +12,31 @@ export function renderPlaylist(parentItem) {
 
   const btnList = document.getElementById("btnList");
   const btnGrid = document.getElementById("btnGrid");
+  const btnSort = document.getElementById("buttonSort");
 
   btnList.addEventListener("click", () => {
-    container.innerHTML = ""
+    container.innerHTML = "";
     btnList.classList.add("active");
     btnGrid.classList.remove("active");
     renderTracks(container);
   });
 
   btnGrid.addEventListener("click", () => {
-    container.innerHTML = ""
+    container.innerHTML = "";
     btnGrid.classList.add("active");
     btnList.classList.remove("active");
     renderCategories(container);
   });
 
+  if (btnList.classList.contains("active")) {
+    renderTracks(container);
+  } else {
+    null;
+  }
 
-  btnList.classList.contains("active") ? renderTracks(container) : null
-
-  console.log(btnList);
-
-  // stateGrid.addEventListener("click", () => {
-  //   container.innerHTML = null;
-  //   setTracks(renderCategories(container));
-  // });
-
-  // if (stateList.classList.contains("active")) {
-  //   container.innerHTML = "";
-
-  //   setTracks(renderTracks(container);)
-  // } else if (stateGrid.classList.contains("active")) {
-  //   container.innerHTML = "";
-  //   renderCategories(container);
-  //   setTracks()
-  // }
-
-  // const observer = new MutationObserver(function (mutationsList) {
-  //   for (let mutation of mutationsList) {
-  //     if (mutation.target === stateList || mutation.target === stateGrid) {
-  //       if (stateList.classList.contains("active")) {
-  //         renderTracks(container);
-  //       } else {
-  //         renderCategories(container);
-  //       }
-  //     }
-  //   }
-  // });
-
-  // observer.observe(stateList, {attributes: true})
-  // observer.observe(stateGrid, {attributes: true})
-
-  // console.log(stateGrid);
+  btnSort.addEventListener("click", () => {
+    setSortSongs(getPlaylist());
+  });
 
   parentItem.appendChild(playlistWrapperElement);
 }
