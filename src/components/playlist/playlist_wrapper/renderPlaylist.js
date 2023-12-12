e69c3dac1fbd0ac4e775ed0f8a35616283f9fa23
Andrey Sery <andrey.72006@gmail.com>
2023-12-12 22:27:30 +0300

    create eventCliks (adding to localStorage)

--- src/components/playlist/playlist_wrapper/renderPlaylist.js
+++ src/components/playlist/playlist_wrapper/renderPlaylist.js
@@ -1,7 +1,6 @@
-import { getPlaylist, setSortSongs } from "../../../data.js";
-import { renderCategories } from "../../cadigories/renderCadigories.js";
 import { containerElement } from "../../containerElement.js";
-import { renderTracks } from "./../../playlist/tracks/renderTracks.js";
+import { sortName } from "../../sort/sortName.js";
+import { eventClicks } from "./eventClicks.js";
 
 export function renderPlaylist(parentItem) {
   const playlistWrapperElement = document.createElement("section");
@@ -10,33 +9,8 @@ export function renderPlaylist(parentItem) {
   const container = containerElement();
   playlistWrapperElement.appendChild(container);
 
-  const btnList = document.getElementById("btnList");
-  const btnGrid = document.getElementById("btnGrid");
-  const btnSort = document.getElementById("buttonSort");
-
-  btnList.addEventListener("click", () => {
-    container.innerHTML = "";
-    btnList.classList.add("active");
-    btnGrid.classList.remove("active");
-    renderTracks(container);
-  });
-
-  btnGrid.addEventListener("click", () => {
-    container.innerHTML = "";
-    btnGrid.classList.add("active");
-    btnList.classList.remove("active");
-    renderCategories(container);
-  });
-
-  if (btnList.classList.contains("active")) {
-    renderTracks(container);
-  } else {
-    null;
-  }
-
-  btnSort.addEventListener("click", () => {
-    setSortSongs(getPlaylist());
-  });
+  eventClicks(container);
+  sortName();
 
   parentItem.appendChild(playlistWrapperElement);
 }
--- src/components/sort/sortName.js
+++ src/components/sort/sortName.js
@@ -1,3 +1,20 @@
+import { getSortDiraction, setSortSongs } from "../../data.js";
+
 export function sortName() {
-  
+  const btnSort = document.getElementById("buttonSort");
+  const b = document.querySelector(".content__add-btn");
+  const iconArrow = btnSort.querySelector("svg");
+  let clickCount = 0;
+  let directionState = getSortDiraction();
+
+  btnSort.addEventListener("click", () => {
+    setSortSongs("ascending");
+    // console.log(getSortDiraction());
+  });
+
+  b.addEventListener("click", () => {
+    setSortSongs("decsnding");
+
+    // console.log(getSortDiraction());
+  });
 }
