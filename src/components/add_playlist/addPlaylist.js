36b5ad6618a16e8122c23fbb79472601f5213f04
Andrey Sery <andrey.72006@gmail.com>
2023-12-27 18:48:56 +0300

    deleted a comment from the addPlaylist

--- src/components/add_playlist/addPlaylist.js
+++ src/components/add_playlist/addPlaylist.js
@@ -28,9 +28,8 @@ export function addPlaylist(parentItem) {
   previewFilesElement.classList.add("modal__list-songs");
 
   const formElement = document.createElement("form");
-  formElement.classList.add("modal__form")
-  
-  
+  formElement.classList.add("modal__form");
+
   const fieldForNamePlaylist = document.createElement("input");
   fieldForNamePlaylist.classList.add("btn", "modal__description");
   fieldForNamePlaylist.type = "text";
@@ -88,88 +87,14 @@ export function addPlaylist(parentItem) {
   });
 
   buttonDownload.addEventListener("click", () => {
-    fieldForNamePlaylist.addEventListener("submit", (event)=> event.preventDefault())
+    fieldForNamePlaylist.addEventListener("submit", (event) =>
+      event.preventDefault()
+    );
     let newNamePlaylist = fieldForNamePlaylist.value;
-    
+
     createNewPlaylist(newNamePlaylist, returnInfoSongs());
-    
+
     modalElement.classList.remove("active");
     document.querySelector("body").classList.remove("hidden");
   });
 }
-
-//
-// export function setupPlaylistModal(parentItem) {
-//   const addButton = document.querySelector(".content__add-btn");
-
-//   const modalElement = createModalElement();
-//   parentItem.append(modalElement);
-
-//   modalElement.addEventListener("click", closeModal);
-
-//   addButton.addEventListener("click", openModal);
-// }
-
-// function createModalElement() {
-//   const modalElement = document.createElement("section");
-//   modalElement.classList.add("modal");
-
-//   const contentElement = document.createElement("div");
-//   contentElement.classList.add("modal__content");
-
-//   const headerElement = document.createElement("div");
-//   headerElement.classList.add("modal__header");
-
-//   const buttonCloseElement = createCloseButtonElement();
-
-//   const titleElement = document.createElement("h2");
-//   titleElement.classList.add("modal__title");
-//   titleElement.textContent = "Create your own playlist!";
-
-//   const inputFileElement = createFileInputElement();
-
-//   const buttonDownload = createDownloadButtonElement();
-
-//   headerElement.append(titleElement);
-//   contentElement.append(buttonCloseElement);
-//   contentElement.append(headerElement);
-//   contentElement.append(inputFileElement);
-//   contentElement.append(buttonDownload);
-//   modalElement.append(contentElement);
-
-//   return modalElement;
-// }
-
-// function createCloseButtonElement() {
-//   const buttonCloseElement = document.createElement("button");
-//   buttonCloseElement.classList.add("btn", "modal__btn-close");
-//   buttonCloseElement.textContent = "X";
-//   buttonCloseElement.addEventListener("click", closeModal);
-//   return buttonCloseElement;
-// }
-
-// function createFileInputElement() {
-//   const inputFileElement = document.createElement("input");
-//   inputFileElement.classList.add("btn", "modal__add-file");
-//   inputFileElement.type = "file";
-//   return inputFileElement;
-// }
-
-// function createDownloadButtonElement() {
-//   const buttonDownload = document.createElement("button");
-//   buttonDownload.classList.add("btn");
-//   buttonDownload.textContent = "Download";
-//   return buttonDownload;
-// }
-
-// function closeModal() {
-//   const modalElement = document.querySelector(".modal");
-//   modalElement.classList.remove("active");
-//   document.querySelector("body").classList.remove("hidden");
-// }
-
-// function openModal() {
-//   const modalElement = document.querySelector(".modal");
-//   modalElement.classList.add("active");
-//   document.querySelector("body").classList.add("hidden");
-// }
