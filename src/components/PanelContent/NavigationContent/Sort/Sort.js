1ea3684014ef42e264dbef2b6b64fb354d99f4e9
Andrey Sery <andrey.72006@gmail.com>
2024-01-03 00:31:24 +0300

    Sort

--- src/components/PanelContent/NavigationContent/Sort/Sort.js
+++ src/components/PanelContent/NavigationContent/Sort/Sort.js
@@ -1,4 +1,27 @@
-export function Sort() {
+import { iconElement } from "../../../iconElement.js";
 
-  return 
+const icon = iconElement("src/assets/images/sprite-icon.svg#arrow-up");
+
+export function Sort() {
+  const sortElement = document.createElement("div");
+  sortElement.classList.add("sort");
+
+  const titleElement = document.createElement("span");
+  titleElement.classList.add("sort__title");
+  titleElement.textContent = "sort by:";
+
+  const buttonElement = document.createElement("button");
+  buttonElement.classList.add("sort__btn");
+  buttonElement.type = "button";
+  buttonElement.id = "buttonSort";
+
+  const textElement = document.createElement("span");
+  textElement.textContent = "name";
+
+  buttonElement.append(textElement);
+  buttonElement.append(icon);
+  sortElement.append(titleElement);
+  sortElement.append(buttonElement);
+
+  return sortElement;
 }
