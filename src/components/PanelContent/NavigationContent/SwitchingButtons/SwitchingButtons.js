734bd179b515ec6f6faf23679660185ae458e9b4
Andrey Sery <andrey.72006@gmail.com>
2024-01-03 00:36:38 +0300

    SwitchingButtons

--- src/components/PanelContent/NavigationContent/SwitchingButtons/SwitchingButtons.js
+++ src/components/PanelContent/NavigationContent/SwitchingButtons/SwitchingButtons.js
@@ -1,4 +1,28 @@
-export function SwitchingButtons() {
+import { iconElement } from "../../../iconElement.js";
 
-  return 
+const iconListElement = iconElement(
+  "src/assets/images/sprite-icon.svg#icon-list"
+);
+
+const iconGridElement = iconElement(
+  "src/assets/images/sprite-icon.svg#icon-grid"
+);
+
+export function SwitchingButtons() {
+  const buttonsElement = document.createElement("div");
+  buttonsElement.classList.add("panel__buttons");
+
+  const buttonListElement = document.createElement("button");
+  buttonListElement.classList.add("btn", "btn--view-module", "active");
+  buttonListElement.id = "btnList";
+
+  const buttonGridElement = document.createElement("button");
+  buttonGridElement.classList.add("btn", "btn--view-module");
+  buttonGridElement.id = "btnGrid";
+
+  buttonListElement.append(iconListElement);
+  buttonGridElement.append(iconGridElement);
+  buttonsElement.append(buttonListElement);
+  buttonsElement.append(buttonGridElement);
+  return buttonsElement;
 }
