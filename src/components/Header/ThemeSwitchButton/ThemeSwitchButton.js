898cb5ab4cbf21606b78f0fc2a13e7f34875e823
Andrey Sery <andrey.72006@gmail.com>
2024-01-03 00:42:52 +0300

    fixed the SwitchButton in ThemeSwitchButton

--- src/components/Header/ThemeSwitchButton/ThemeSwitchButton.js
+++ src/components/Header/ThemeSwitchButton/ThemeSwitchButton.js
@@ -1,15 +1,15 @@
+import { AccentButton } from "../../AccentButton/AccentButton.js";
 import { iconElement } from "./../../iconElement.js";
 import { renderTheme } from "./utils/renderTheme.js";
 import { switchingTheme } from "./utils/switchingTheme.js";
 
 const icon = iconElement("src/assets/images/sprite-icon.svg#icon-moon");
 const colorsKit = document.querySelector("body");
+const switchButton = AccentButton();
+switchButton.classList.add("btn--toggle-theme");
 
 export function ThemeSwitchButton(logoElement) {
   const imageLogo = logoElement.querySelector(".logo");
-
-  const switchButton = document.createElement("button");
-  switchButton.classList.add("btn", "btn--toggle-theme");
 
   let currentTheme = localStorage.getItem("currentTheme");
 
